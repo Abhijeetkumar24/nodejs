79424a15350fff852808329d44761ca264d95556
Abhijeet <abhijeet.kumar@appinventiv.com>
2023-07-03 21:54:13 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("This is main branch");
+const express =require('express');
+let bodyParser=require('body-parser');
+
+const app =express();
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+
+app.post('/add', (req,res)=>{
+    const {num1 , num2} =req.body;
+    let add=num1+num2;
+    res.send(result);
+})
+
+app.listen(3000 ,()=>{console.log("server start at 3000")});

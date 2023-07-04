a30129d1a7f79ea6e5ae2f06a8023beb5c6154b5
Abhijeetkumar24 <137895993+Abhijeetkumar24@users.noreply.github.com>
2023-07-04 15:01:28 +0530

    Update abhijeet index.js

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-const express =require('express');
-let bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app =express();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    res.send("hello world");
+app.post('/add', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = Number(num1) + Number(num2);
+    res.json({ result });
+    res.send(result);
+  });
+
+app.post('/subtract', (req, res) => {
+  const { num1, num2 } = req.body;
+  const result = Number(num1) - Number(num2);
+  res.json({ result });
 });
 
-app.post('/add', (req,res)=>{
-    const {num1 , num2} =req.body;
-    let add=num1+num2;
-    res.send(add);
-})
+app.post('/multiply', (req, res) => {
+  const { num1, num2 } = req.body;
+  const result = Number(num1) * Number(num2);
+  res.json({ result });
+});
 
-app.listen(3000 ,()=>{console.log("server start at 3000")});
+app.post('/divide', (req, res) => {
+  const { num1, num2 } = req.body;
+  const result = Number(num1) / Number(num2);
+  res.json({ result });
+});
+
+app.post('/power', (req, res) => {
+  const { base, exponent } = req.body;
+  const result = Math.pow(Number(base), Number(exponent));
+  res.json({ result });
+});
+
+app.post('/square-root', (req, res) => {
+  const { number } = req.body;
+  const result = Math.sqrt(Number(number));
+  res.json({ result });
+});
+
+app.post('/percentage', (req, res) => {
+  const { num1, num2 } = req.body;
+  const result = (Number(num1) / Number(num2)) * 100;
+  res.json({ result });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
